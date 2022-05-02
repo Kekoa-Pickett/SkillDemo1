6a720eef0e030e07e83c68f9260a8cb65323266f
Kekoa-Pickett <103221420+Kekoa-Pickett@users.noreply.github.com>
2022-05-01 17:39:09 -0700

    Multiply

---
+++ SkillDemo1Tester.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemo1Tester {
+    @Test 
+    public void multiplyTester() {
+        assertEquals(2, SkillDemo1.multiply(1, 1));
+    }
+}
