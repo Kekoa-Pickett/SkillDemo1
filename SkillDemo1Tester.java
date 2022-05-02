477093f77218d3df159cb5303c9430dcebb63eaf
Kekoa-Pickett <103221420+Kekoa-Pickett@users.noreply.github.com>
2022-05-01 17:43:36 -0700

    Update SkillDemo1Tester.java

--- SkillDemo1Tester.java
+++ SkillDemo1Tester.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class SkillDemo1Tester {
     @Test 
     public void multiplyTester() {
-        assertEquals(2, SkillDemo1.multiply(1, 1));
+        assertEquals(1, SkillDemo1.multiply(1, 1));
     }
 }
